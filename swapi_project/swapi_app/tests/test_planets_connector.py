e993843ec469f67f4c33e7ff9d7bdd5f1415b2d7
Ewelina Szmielew <ewelina@szmielew.pl>
2023-03-02 15:00:29 +0100

    Test change for planets generator to generate more data

--- swapi_project/swapi_app/tests/test_planets_connector.py
+++ swapi_project/swapi_app/tests/test_planets_connector.py
@@ -7,36 +7,69 @@ EXPECTED_DATA = [
     ["Tatooine", "https://swapi.dev/api/planets/1/"],
     ["Alderaan", "https://swapi.dev/api/planets/2/"],
     ["Yavin IV", "https://swapi.dev/api/planets/3/"],
+    ["Tatooine", "https://swapi.dev/api/planets/1/"],
+    ["Yavin IV", "https://swapi.dev/api/planets/3/"],
 ]
 
 
 class Response:
+    def __init__(self, call_count):
+        self.call_count = call_count
+
     def json(self):
-        return {
-            "next": None,
-            "results": [
-                {
-                    "name": "Tatooine",
-                    "rotation_period": "23",
-                    "url": "https://swapi.dev/api/planets/1/",
-                },
-                {
-                    "name": "Alderaan",
-                    "rotation_period": "24",
-                    "url": "https://swapi.dev/api/planets/2/",
-                },
-                {
-                    "name": "Yavin IV",
-                    "rotation_period": "24",
-                    "url": "https://swapi.dev/api/planets/3/",
-                },
-            ],
-        }
+        data = [
+            {
+                "next": "next",
+                "results": [
+                    {
+                        "name": "Tatooine",
+                        "rotation_period": "23",
+                        "url": "https://swapi.dev/api/planets/1/",
+                    },
+                    {
+                        "name": "Alderaan",
+                        "rotation_period": "24",
+                        "url": "https://swapi.dev/api/planets/2/",
+                    },
+                    {
+                        "name": "Yavin IV",
+                        "rotation_period": "24",
+                        "url": "https://swapi.dev/api/planets/3/",
+                    },
+                ],
+            },
+            {
+                "next": "next",
+                "results": [
+                    {
+                        "name": "Tatooine",
+                        "rotation_period": "23",
+                        "url": "https://swapi.dev/api/planets/1/",
+                    },
+                ],
+            },
+            {
+                "next": None,
+                "results": [
+                    {
+                        "name": "Yavin IV",
+                        "rotation_period": "24",
+                        "url": "https://swapi.dev/api/planets/3/",
+                    },
+                ],
+            },
+        ]
+        return data[self.call_count]
 
 
 class PlanetsRequestMock:
+    def __init__(self):
+        self.counter = 0
+
     def get(self, _):
-        return Response()
+        response = Response(self.counter)
+        self.counter += 1
+        return response
 
 
 class SwapiPlanetsConnectorTest(TestCase):
